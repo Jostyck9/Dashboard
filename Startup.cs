74fae2264c32fd8f3ef505ab994c4669e2364993
Jostyck9 <hugo.berthome@epitech.eu>
2019-11-05 22:19:14 +0100

    adding Facebook authentification

--- Startup.cs
+++ Startup.cs
@@ -52,7 +52,12 @@ namespace Dashboard
                 {
                     microsoftOptions.ClientId = Configuration["Authentication:Microsoft:ClientId"];
                     microsoftOptions.ClientSecret = Configuration["Authentication:Microsoft:ClientSecret"];
-                });
+                })
+                .AddFacebook(facebookOptions =>
+                {
+                    facebookOptions.AppId = Configuration["Authentication:Facebook:AppId"];
+                    facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
+                }); ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
