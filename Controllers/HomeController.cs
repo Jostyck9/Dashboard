aeed76efe74edc5a20a6d391faa995c9e53c33cf
Jostyck9 <hugo.berthome@epitech.eu>
2019-11-11 16:20:01 +0100

    adding all the widget on the index page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,23 +7,40 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Dashboard.Models;
 using Microsoft.AspNetCore.Authorization;
+using Dashboard.Models.Widgets;
+using Dashboard.Models.Widgets.Youtube;
+using Dashboard.Models.youtube;
+using Dashboard.Models.Weather;
+using Dashboard.Models.Widgets.Weather;
+using Dashboard.Models.Steam;
+using Dashboard.Models.Widgets.Steam;
 
 namespace Dashboard.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IYoutubeModel YtModel = new YoutubeModel();
+        private readonly IWeatherModel WModel = new WeatherModel();
+        private readonly ISteamModel SModel = new SteamModel();
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            Dal test = new Dal();
-            DataTest toAdd = test.getData();
-            return View(toAdd);
+            List<IWidget> res = new List<IWidget>();
+            if (HttpContext.User.Identity.IsAuthenticated) {
+                res.Add(new WidgetVideoYoutube { Data = await YtModel.GetVideoById("wZZ7oFKsKzY") });
+                res.Add(new WidgetChannelYoutube { Data = await YtModel.GetChannelById("UCYGjxo5ifuhnmvhPvCc3DJQ") });
+                res.Add(new WidgetWeather { Data = await WModel.GetWeatherByLocation("London") });
+                res.Add(new WidgetNewsSteam { Data = await SModel.GetLastNews("440", 2) });
+                res.Add(new WidgetPlayersGameSteam { Data = new GameData { data = await SModel.GetCurrentPlayersGame("440"), Banner = SModel.GetGameBanner("440") } });
+                res.Add(new WidgetAchievementsSteam { Data = await SModel.GetAchievementGame("440") });
+            }
+            return View(res);
         }
 
         public IActionResult Privacy()
