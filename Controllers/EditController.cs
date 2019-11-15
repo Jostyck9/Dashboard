b11a7a86ce1ae3c4909d42cbad6df1dd1c254896
Jostyck9 <hugo.berthome@epitech.eu>
2019-11-15 16:52:28 +0100

    adding the edition for a youtube video

--- Controllers/EditController.cs
+++ Controllers/EditController.cs
@@ -2,15 +2,54 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dashboard.Models.youtube;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Dashboard.Data;
+using Dashboard.Models.WidgetsSettings;
+using Dashboard.Models.Widgets;
 
 namespace Dashboard.Controllers
 {
     public class EditController : Controller
     {
+        private IDal _widgetsSettings;
+        private readonly IYoutubeModel _ytModel;
+
+        public EditController(ApplicationDbContext db)
+        {
+            _widgetsSettings = new Dal(db);
+            _ytModel = new YoutubeModel();
+        }
+
+        [HttpGet, Route("Edit")]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<bool> AddChannelYoutube(string url)
+        {
+            return true;
+        }
+
+        [HttpPost, Authorize]
+        public async Task<bool> AddVideoYoutube(string url)
+        {
+            VideoYoutube res = await _ytModel.GetVideoByUrl(url);
+
+            if (res.VideoId == "")
+            {
+                return false;
+            }
+            var currentUser = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (currentUser != null)
+            {
+                _widgetsSettings.AddWidget(currentUser, WidgetsId.YOUTUBE_VIDEO, res.VideoId);
+            }
+            return true;
         }
     }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(function () {
+    $('label#error_urlVideo').hide();
+    $('.alert').hide();
+    $("button#channel_submit").click(function () {
+        $('.alert-danger').hide();
+        var url = $("input#urlVideo").val();
+        if (url == "") {
+            $(".alert-danger").show();
+            $("input#urlVideo").focus();
+            return false;
+        }
+        $.ajax({
+            type: "POST",
+            url: "Edit/AddVideoYoutube",
+            data: "url=" + url,
+            success: function () {
+                $('div#succes_video').show("fast");
+            },
+            error: function () {
+                $('div#error_video').show("fast");
+            }
+        });
+        return false;
+    });
+});
