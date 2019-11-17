095b100a309820a8bd36b2403a6ab3d226919059
Jostyck9 <hugo.berthome@epitech.eu>
2019-11-17 19:08:18 +0100

    adding the old unit tests to the solution

---
+++ XUnitTest/TestWeatherModel.cs
@@ -0,0 +1,38 @@
+﻿using Dashboard.Models.Weather;
+using Xunit;
+
+namespace XUnitTest
+{
+    public class TestWeatherModel
+    {
+        private readonly WeatherModel _wModel = new WeatherModel();
+
+        [Fact]
+        public async void TestgetWeatherByLocation()
+        {
+            var res = await _wModel.GetWeatherByLocation("Nantes");
+
+            Assert.Equal("Nantes", res.Name);
+            Assert.NotEqual("", res.Main.Temp);
+            Assert.NotEmpty(res.Weather);
+
+            res = await _wModel.GetWeatherByLocation("azydguziehoizjefoijzoiefjoizejfoizjeoifj");
+
+            Assert.Null(res.Name);
+        }
+
+        [Fact]
+        public async void TestgetWeatherByCoord()
+        {
+            var res = await _wModel.GetWeatherByCoord(20, 20);
+
+            Assert.Equal("", res.Name);
+            Assert.NotEqual("", res.Main.Temp);
+            Assert.NotEmpty(res.Weather);
+
+            res = await _wModel.GetWeatherByCoord(987198, 8118212112);
+
+            Assert.Null(res.Name);
+        }
+    }
+}
